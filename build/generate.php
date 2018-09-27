84397ae8f9facd06d01ca1cc6de4ca47056012fb
Sebastian Bergmann <sb@sebastian-bergmann.de>
2018-09-27 18:23:39 +0800

    Fix

--- build/generate.php
+++ build/generate.php
@@ -33,7 +33,6 @@ $buffer = <<<EOT
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace SebastianBergmann\ResourceOperations;
 
 final class ResourceOperations
